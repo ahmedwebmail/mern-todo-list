c0c6179b32e7245da53e35a30acfec15a74b6491
ahmedwebmail <aakhaled.msc@gmail.com>
2024-09-11 15:18:36 +0100

    configure settings for sending email

--- app/config/config.js
+++ app/config/config.js
@@ -3,22 +3,24 @@ export const DATABASE = "mongodb+srv://csewebmail:12345@cluster0.nx8kf.mongodb.n
 export const JWT_KEY = "ABCD12345"
 export const JWT_EXPIRE_TIME = 30*24*60*60*1000
 
-//Email settings
-export const EMAIL_HOST = ""
-export const EMAIL_PORT = ""
-export const EMAIL_USER = ""
-export const EMAIL_PASS = ""
+// Email settings
+export const EMAIL_HOST = "sandbox.smtp.mailtrap.io"
+export const EMAIL_PORT = 25
+export const EMAIL_SECURITY = false
+export const EMAIL_USER = "e1906862bde9c1"
+export const EMAIL_PASS = "04cbfbb703d740"
+export const EMAIL_UNAUTH = false
 
-//Cacheing
+// Cacheing
 export const WEB_CACHE = false
 
-//Json file settings
+// Json file settings
 export const MAX_JSON_SIZE = "20MB"
 
-//URL settings
+// URL settings
 export const URL_ENCODE = true
 
-//Req settings
+// Req settings
 export const REQUEST_TIME = 20*60*1000
 export const REQUEST_NUM  = 2000
 
