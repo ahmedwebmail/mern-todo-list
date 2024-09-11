4f11857190afdc22ed09b43f216dd91e587943b5
ahmedwebmail <aakhaled.msc@gmail.com>
2024-09-11 15:41:41 +0100

    create task model

--- app/model/UserModel.js
+++ app/model/UserModel.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema({
-        first_name: {type: String},
-        last_name: {type: String},
+        first_name: {type: String,required: true},
+        last_name: {type: String, required: true},
         email: {type: String, unique: true},
-        password: {type: String}
+        password: {type: String, required: true},
+        OTP: {type: String, default: 0},
     },
     {
         timestamps: true,
